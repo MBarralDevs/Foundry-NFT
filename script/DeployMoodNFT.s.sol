73184f25b9bee27ef81fcb490ab7203b033e7996
mbarraldevs <mbarraldevs@outlook.com>
2025-01-08 10:17:38 +0100

    first commit

---
+++ script/DeployMoodNFT.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MoodNFT} from "../src/MoodNFT.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNFT is Script {
+    function run() external returns (MoodNFT) {
+        string memory sadSvg = vm.readFile("./img/sad.svg");
+        string memory happySvg = vm.readFile("./img/happy.svg");
+
+        vm.startBroadcast();
+        MoodNFT moodNft = new MoodNFT(
+            svgToImageURI(sadSvg),
+            svgToImageURI(happySvg)
+        );
+        vm.stopBroadcast();
+        return moodNft;
+    }
+
+    function svgToImageURI(
+        string memory svg
+    ) public pure returns (string memory) {
+        string memory baseURI = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg))) // Removing unnecessary type castings, this line can be resumed as follows : 'abi.encodePacked(svg)'
+        );
+        return string(abi.encodePacked(baseURI, svgBase64Encoded));
+    }
+}
